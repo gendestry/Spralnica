aec5ed6cbd49390a96f72d7da37a7adbe7aad5d2
Lan Vukušič <lan.vukusic@gmail.com>
2022-11-08 18:34:47 +0100

    functions test

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
-import * as functions from "firebase-functions";
+import { https, Request, Response, logger } from "firebase-functions";
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const helloWorld = https.onRequest(
+  (request: Request, response: Response) => {
+    logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
+  }
+);
