04a656fad06eca189e37c1886def2f97ce18a0cf
Lan Vukušič <lan.vukusic@gmail.com>
2022-11-18 14:11:04 +0100

    added registeruser func

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -8,6 +8,7 @@ import { merge } from "lodash";
 import { https, logger, Request, Response } from "firebase-functions";
 import * as admin from "firebase-admin";
 import { applicationDefault } from "firebase-admin/app";
+
 // import { defaultAuth, defaultDatabase } from "./firebase";
 
 // firebase stuff
@@ -68,45 +69,91 @@ export const allUsers = https.onRequest(
   }
 );
 
-// when a user is successfully registered, call this endpoint to initialize the user
-export const postRegister = https.onRequest(
+export const registerUser = https.onRequest(
   (request: Request, response: Response) => {
     const data = request.body;
-    logger.info(data, { structuredData: true });
-    // read: "uuid", "name", "surname", "room" from data
-    const uuid: string = data.uuid;
+    // const uuid: string = data.uuid;
+    const email: string = data.email;
     const name: string = data.name;
-    const surname: string = data.surname;
+    const surname: string = data.string;
     const room: number = data.room;
+    const password: string = data.password;
+    const phone: string = data.phone;
 
-    const defaultDatabase = getFirestore();
-    // create a new document in the "users" collection
-    defaultDatabase
-      .collection("users")
-      .add({
-        uuid,
-        name,
-        surname,
-        room,
-        confirmed: false,
+    const defaultAuth = getAuth();
+    defaultAuth
+      .createUser({
+        // await.....
+        email: email,
+        password: password,
+        phoneNumber: phone,
       })
-      .then(() => {
-        response.send("success");
-      })
-      .catch((error) => {
-        // user could not be initialized. Remove the user from the auth list
-        const defaultAuth = getAuth();
+      .then((user) => {
+        // console.log(user);
+        // response.sendStatus(200);
         defaultAuth
-          .deleteUser(uuid)
-          .then(() => {
-            response.status(500).send(error);
+          .setCustomUserClaims(user.uid, {
+            name: name,
+            surname: surname,
+            room: room,
+            role: "user",
           })
-          .catch((error) => {
-            response.status(500).send(error);
+          .then(() => {
+            response.sendStatus(200);
+          })
+          .catch((err: Error) => {
+            response
+              .status(503)
+              .send(JSON.stringify({ step: "claim", ...err }));
           });
+      })
+      .catch((err: Error) => {
+        response.status(503).send(JSON.stringify({ step: "create", ...err }));
       });
+
+    // const defaultDatabase = getFirestore();
   }
 );
+
+// when a user is successfully registered, call this endpoint to initialize the user
+// export const postRegister = https.onRequest(
+//   (request: Request, response: Response) => {
+//     const data = request.body;
+//     logger.info(data, { structuredData: true });
+//     // read: "uuid", "name", "surname", "room" from data
+//     const uuid: string = data.uuid;
+//     const name: string = data.name;
+//     const surname: string = data.surname;
+//     const room: number = data.room;
+
+//     const defaultDatabase = getFirestore();
+//     // create a new document in the "users" collection
+//     defaultDatabase
+//       .collection("users")
+//       .add({
+//         uuid,
+//         name,
+//         surname,
+//         room,
+//         confirmed: false,
+//       })
+//       .then(() => {
+//         response.send("success");
+//       })
+//       .catch((error) => {
+//         // user could not be initialized. Remove the user from the auth list
+//         const defaultAuth = getAuth();
+//         defaultAuth
+//           .deleteUser(uuid)
+//           .then(() => {
+//             response.status(500).send(error);
+//           })
+//           .catch((error) => {
+//             response.status(500).send(error);
+//           });
+//       });
+//   }
+// );
 
 // toggle user enabled
 export const setConfirmed = https.onRequest(
